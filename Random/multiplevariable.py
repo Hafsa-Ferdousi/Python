d0e8e478db1c91f299dd2a1aaf706a84544b4172
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-02-04 17:37:59 +0600

    Add files via upload

---
+++ Random/multiplevariable.py
@@ -0,0 +1,32 @@
+x,y,z="mango","banana","Apple"
+print(x); print(z); print (y)
+
+print (z)
+print(x)
+
+print(" ".join([x,y,z]))
+
+animal=["dog","cat"]
+a,b=animal
+print(animal)
+print(a)
+print(" ".join([b,a]))
+print( " ".join(animal))
+
+print(" ".join([x,y,z]), end= " ")
+#print without a newline
+print(" ".join(animal))
+
+language="c","python","java"
+p="c "
+q="python " #have to use space otherwise output will be without space
+r="java "
+print(p + q + r)
+
+m=10
+n=2
+print(n + m )
+print("add= " + str(m+n ))
+print("add={} ".format(m+n))
+
+
