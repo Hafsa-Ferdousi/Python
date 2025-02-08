cc107316f60e4d966689f20d52f759be2f627c2d
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-02-08 19:38:53 +0600

    Add files via upload

---
+++ Random/Expression.py
@@ -0,0 +1,35 @@
+print(1.33+1)
+
+x,y=2.3443,4
+print(x+y)
+print(x/y)
+print(y//x)
+print(type(y//x))
+h=1.6+3.6
+print(h)
+
+print(2**3)  #Exponentiation
+
+print(10==9) #False
+
+#relational...
+
+d,f,g=1.2,5,6
+
+
+p=(d*f)+g*f+d == d*f+f
+print(p)
+
+ab,ac=2,3
+t="Hafsa"
+print(ac*ab*t)
+ab,ac="2",3
+t="Hafsa"
+#print(ac*ab*t) #error
+
+print(ac*ab+t) # string and string can operate with+
+#output 222Hafsa
+
+
+print(-5%2)
+print(5%-2)
