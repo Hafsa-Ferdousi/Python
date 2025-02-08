b20de27344dbccb6c45bd8b453aed487bcfff172
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-02-09 04:45:47 +0600

    Add files via upload

---
+++ Random/Number.py
@@ -0,0 +1,19 @@
+import random
+print(random.randrange(1,10))
+
+num=random.randint(1,15)
+print(num)
+
+n=random.uniform(1.56,12) #float
+print(n)
+
+
+numberR=random.random() #random float between 0 and 1
+print(numberR)
+
+
+A=random.randrange(10,25,5) #random number form10,15,20...
+print(A)
+
+z=1j
+print(type(z))
---
+++ Random/input.py
@@ -0,0 +1,11 @@
+
+print (int( input("age: ")))
+
+
+name=input("Enter your name:")
+Age=int(input("Age: "))
+print(name ," is " ,Age," years old")
+
+
+x,y=map(float ,input("Enter the number: ").split())
+print("Sum: ",x+y)
