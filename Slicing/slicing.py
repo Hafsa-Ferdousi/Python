bd31e0359d5ba7ecc57fe74bcd8b4b5625fd4ac9
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-02-14 16:01:20 +0600

    Add files via upload

---
+++ Slicing/slicing.py
@@ -0,0 +1,20 @@
+#reversing
+
+a="Science"
+
+print(a[::-1]) #backward
+
+print(a[::-2])
+
+print(a[:1])
+
+print(a[1:7:2]) #every 2nd character from index 1to 7(exclusive)
+
+print(a[:-3])#start to -3(excluding)
+
+print(a[-2:-1])
+print(a[-1:-1])
+print(a[:-1])
+print(a[-1:])
+print(a[-3:-1])
+
