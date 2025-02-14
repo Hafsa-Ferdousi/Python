dbc27cbb0a3c900943f3303762dcb8af164d6afc
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-02-14 13:43:26 +0600

    Add files via upload

---
+++ Slicing/Slicing Strings.py
@@ -0,0 +1,12 @@
+a="Hafsa Ferdousi"
+print(a[1:7]) # position 1 to 7 ,output:afsa F
+
+print(a[:2]) #from start
+
+print(a[4:]) #from position to 4 to the end
+
+print(a[-5])
+print(a[-5:])
+print(a[:-5])
+
+print(a[-5:-2])
