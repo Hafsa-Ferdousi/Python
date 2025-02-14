430904a1997608f43ff228c7d77b7217a746384c
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-02-14 22:33:54 +0600

    Add files via upload

---
+++ Conditional/Light.py
@@ -0,0 +1,13 @@
+light=input("Light: ")
+if(light=="red"):
+    print("Stop")
+
+elif(light=="yellow"):
+    print("wait until green")
+elif(light=="green"):
+    print("go")
+else:
+    print("light is broken")
+
+
+
