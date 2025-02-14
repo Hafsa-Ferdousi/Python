ba1cd808a15f3a5de70b65fc2cda82b351ce1589
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-02-14 18:30:11 +0600

    Add files via upload

---
+++ Methods/capitalize.py
@@ -0,0 +1,11 @@
+a="miraj.mehedi"
+
+print(a.capitalize())
+b=a.capitalize()
+print(b)
+#capitalize() Upper case the first letter in this sentence
+
+text= "i am Miraj C"
+print(text.capitalize())
+
+#The first character is converted to upper case, and the rest are converted to lower case:
