4dfeb8f19ef9980a8426df92f9a623df268ee618
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-02-15 10:58:34 +0600

    Add files via upload

---
+++ Lists/Listconstructor.py
@@ -0,0 +1,16 @@
+l=list(("M",12,5))
+print(l)
+print(type(l))
+print(len(l))
+print(l[2])
+
+list1=["Tia",True,27.5,0,4]
+list1[3]=False
+print(list1)
+
+
+list1[2]=("Fase")
+print(list1)
+
+list1[0]=(False)
+print(list1)
