3a1ec82802e222adf95d16acdc5ba96a2e4d95ba
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-02-15 14:45:01 +0600

    Add files via upload

---
+++ Lists/SortList.py
@@ -0,0 +1,27 @@
+t=["N","M","J","O"]
+t.sort()
+print(t)
+
+
+n=[12,124,12.56]
+n.sort()
+print(n)
+
+#sort Descending
+
+m=[12,23,43,45,5]
+m.sort(reverse=True)
+print(m)
+
+o=["bie","Her","nwe","New","Kwe"]
+o.sort(key=str.lower)
+print(o)
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort(key = str.lower)
+print(thislist)
+
+h=["dfs",12,3444444]
+h.reverse()
+print(h)
+
