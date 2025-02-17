3ccbe808eda9c0cdc7c514171ef61004e825ba67
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-02-17 19:45:03 +0600

    Add files via upload

---
+++ Dictionaries/dictionaries.py
@@ -0,0 +1,23 @@
+student={
+    "name":"Mina",
+    "Roll":5,
+    "class":4
+
+}
+
+print(student)
+print(student["name"])
+
+
+student2={
+    "name":"rina",
+     "name":"bina",  #Duplicate values will overwrite existing values
+    "Roll":5,
+    "class":4
+
+}
+
+print(student2)
+
+print(len(student2))
+print(type(student2))
