293d2eed88c2b5339a2b38c167ba71f688c89412
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-02-17 19:01:18 +0600

    Add files via upload

---
+++ Sets/set.py
@@ -0,0 +1,28 @@
+a={"mango","banana","apple"}
+print(type(a))
+print(a)
+print(a)
+
+
+aa={"mango","banana","apple", 12.45,2,True,False}
+print(aa)
+
+print(len(aa))
+
+
+f={1,1,True}
+print(f)
+print(len(f))
+
+
+b={1,1,True,12, False,0}
+print(b)
+print(len(b))
+
+
+c={"mango","banana","apple", 1,0,12.45,2,True,False}
+print(c)
+
+d=set(("mina","ds","a","oop"))  #Constructor
+print(d)
+print(type(d))
