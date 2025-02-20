e14235e1f23b396369b216eac2d286dbcba6eee9
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-02-20 23:45:46 +0600

    Add files via upload

---
+++ OOP/class.py
@@ -0,0 +1,19 @@
+class Student:
+ name=""
+ age=""
+
+a = Student()
+a.name="hafsa"
+a.age=21
+
+
+b=Student()
+b.name="Mina"
+b.age=23
+
+print(a.name)
+print(a.age)
+print(b.name + " age "+ str(b.age)) #can only concatenate str (not "int") to str
+
+
+
