69cb7321afbc0397688d052379e8a366f27ea95b
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-02-21 21:52:26 +0600

    Add files via upload

---
+++ OOP/Constructors/Parameterized Constructor.py
@@ -0,0 +1,8 @@
+class House:
+    def __init__(self, kitchen, drawingroom ):
+        self.kitchen=kitchen
+        self.drawingroom =drawingroom
+
+house=House("Cooking","TV")
+print(house.kitchen, )
+print(house.drawingroom)
